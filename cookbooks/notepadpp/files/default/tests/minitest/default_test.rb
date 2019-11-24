b2e328a1a3373109098f2f98482d0d2e70a28d0f
hackforlive <michael.hrabe@gmail.com>
2019-11-24 22:09:28 +0100

    testing chrome

--- cookbooks/notepadpp/files/default/tests/minitest/default_test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/spec'
-
-# Ensure Notepad++ was installed
-class TestNotepadppInstall < MiniTest::Chef::TestCase
-  def test_notepadpp_was_installed
-    assert ::File.exist?(::File.join(node['notepadpp']['install_dir'], 'notepad++.exe'))
-  end
-end
