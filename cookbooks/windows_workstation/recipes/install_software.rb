04fa3fa2b5e6c5184f82c5af18bd3bbef048c619
hackforlive <michael.hrabe@gmail.com>
2019-11-26 21:35:39 +0100

    testing chocolate resource

--- cookbooks/windows_workstation/recipes/install_software.rb
+++ cookbooks/windows_workstation/recipes/install_software.rb
@@ -3,7 +3,7 @@
 # Recipe:: software
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-CHOCOLATE_PACKAGES = default['chocolatey']['packages']
+CHOCOLATE_PACKAGES = node['chocolatey']['packages']
 
 CHOCOLATE_PACKAGES.each do |package_name|
   chocolatey_package package_name do
